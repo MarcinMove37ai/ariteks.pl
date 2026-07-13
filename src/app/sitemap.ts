935961507e6d53bec9f074cd4b667018b55ac7d5
MarcinMove37ai <marcinlisiak83@gmial.com>
2026-07-13 15:13:08 +0200

    Add catalog index pages to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -36,6 +36,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const routes: RoutePair[] = [
     { plPath: '', enPath: '' },
     { plPath: '/about', enPath: '/about' },
+    { plPath: '/applications', enPath: '/applications' },
+    { plPath: '/fabrics', enPath: '/fabrics' },
     { plPath: '/certificates', enPath: '/certificates' },
 
     ...APPLICATIONS.map((app) => ({
